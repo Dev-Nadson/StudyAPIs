54ce2dbff4414e09618d65d2aaa70f1c729ec035
Dev Nadson <nadson.pros@gmail.com>
2025-06-09 16:13:31 -0300

    Fix: Rotas adicionadas e run corrigido

--- PrimeiraAPI/firstAPI.py
+++ PrimeiraAPI/firstAPI.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
+
 app = FastAPI()
-from login_routes import *
-from crud_routes import *
+
+from login_routes import login_router
+from crud_routes import crud_router
+
+app.include_router(login_router)
+app.include_router(crud_router)
 
 #Requisições GET - POST - PUT/PATCH - DELETE
 
 if __name__ == "__main__":
-    uvicorn.run("PrimeiraAPI_Teste:app", host="127.0.0.1", reload=True)
+    uvicorn.run("firstAPI:app", host="127.0.0.1", reload=True)

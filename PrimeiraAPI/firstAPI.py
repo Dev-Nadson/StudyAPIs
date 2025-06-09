fa5141204ece7141775245a54080f853bc0bc15d
Dev Nadson <nadson.pros@gmail.com>
2025-06-09 15:45:27 -0300

    Feat: Arquivos de rotas adicionados

--- PrimeiraAPI/firstAPI.py
+++ PrimeiraAPI/firstAPI.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
 app = FastAPI()
+from login_routes import *
+from crud_routes import *
 
 #Requisições GET - POST - PUT/PATCH - DELETE
-@app.get("/")
-def read_root():
-    return {1: "Hello, World"}
-
-@app.get("/filosofo-pedro")
-def read_root():
-    return {"Pro avião voar ele teve que planar"}
 
 if __name__ == "__main__":
     uvicorn.run("PrimeiraAPI_Teste:app", host="127.0.0.1", reload=True)

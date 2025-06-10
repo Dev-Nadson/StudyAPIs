c2d9e2cde79ca47492226bbee2eaa2624357c43e
Dev Nadson <nadson.pros@gmail.com>
2025-06-10 11:04:13 -0300

    Feat: Rota teste adicionada

--- PrimeiraAPI/login_routes.py
+++ PrimeiraAPI/login_routes.py
@@ -1,2 +1,6 @@
 from fastapi import APIRouter
 login_router = APIRouter(prefix="/login", tags=["login"])
+
+@login_router.get("/")
+async def login():
+    return {"Mensagem": "Rota funcionando", "Status": True}

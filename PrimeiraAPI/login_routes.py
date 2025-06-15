d002b1b240fa12e0077a6ae3656effc940c3bd9b
Dev Nadson <nadson.pros@gmail.com>
2025-06-15 01:42:37 -0300

    Feat: Função de criptografia de senha adicionada

--- PrimeiraAPI/login_routes.py
+++ PrimeiraAPI/login_routes.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
-from sqlalchemy.orm import sessionmaker, declarative_base
-from models import Usuario, database
+from fastapi import APIRouter, Depends
+from models import Usuario
+from dependencies import pegar_sessao, bcrypt_context
+
 login_router = APIRouter(prefix="/login", tags=["login"])
 
 @login_router.get("/")
@@ -8,14 +9,13 @@ async def home():
     return {"Mensagem": "Rota funcionando.", "Status": True}
 
 @login_router.post("/criar_conta")
-async def criar_conta(email: str, senha: str, nome: str): #FastAPI funciona melhor declarando o tipo de dado da variável
-    SessionLocal = sessionmaker(bind=database)
-    session = SessionLocal()
+async def criar_conta(email: str, senha: str, nome: str, session = Depends(pegar_sessao)): #FastAPI funciona melhor declarando o tipo de dado da variável
     usuario = session.query(Usuario).filter(Usuario.email==email).first()
     if usuario:
         return {"mensagem": "Já existe um usuário com esse email."}
     else:
-        novo_usuario = Usuario(nome, email, senha)
+        senha_criptografada = bcrypt_context.hash(senha)
+        novo_usuario = Usuario(nome=nome, email=email, senha=senha_criptografada)
         session.add(novo_usuario)
         session.commit()
         return {"mensagem": "Usuário cadastrado com sucesso."}

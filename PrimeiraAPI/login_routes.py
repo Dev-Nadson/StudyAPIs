e5b9f6f33b866d9cc91fa8f3bcedf7f80a0fe4c3
Dev Nadson <nadson.pros@gmail.com>
2025-06-14 23:04:42 -0300

    Feat: Primeira rota Post adicionada

--- PrimeiraAPI/login_routes.py
+++ PrimeiraAPI/login_routes.py
@@ -1,6 +1,21 @@
 from fastapi import APIRouter
+from sqlalchemy.orm import sessionmaker, declarative_base
+from models import Usuario, database
 login_router = APIRouter(prefix="/login", tags=["login"])
 
 @login_router.get("/")
-async def login():
+async def home():
     return {"Mensagem": "Rota funcionando.", "Status": True}
+
+@login_router.post("/criar_conta")
+async def criar_conta(email: str, senha: str, nome: str): #FastAPI funciona melhor declarando o tipo de dado da variável
+    SessionLocal = sessionmaker(bind=database)
+    session = SessionLocal()
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+    if usuario:
+        return {"mensagem": "Já existe um usuário com esse email."}
+    else:
+        novo_usuario = Usuario(nome, email, senha)
+        session.add(novo_usuario)
+        session.commit()
+        return {"mensagem": "Usuário cadastrado com sucesso."}

34edcbc989017358806164964b249af898ad72b0
Dev Nadson <nadson.pros@gmail.com>
2025-06-15 02:17:27 -0300

    Feat: Substuição de inputs por schemas padronizados

--- PrimeiraAPI/login_routes.py
+++ PrimeiraAPI/login_routes.py
@@ -1,21 +1,22 @@
 from fastapi import APIRouter, HTTPException,Depends
 from models import Usuario
 from dependencies import pegar_sessao, bcrypt_context
+from schemas import UsuarioSchema
+from sqlalchemy.orm import session
 
 login_router = APIRouter(prefix="/login", tags=["login"])
-
 @login_router.get("/")
 async def home():
     return {"Mensagem": "Rota funcionando.", "Status": True}
 
 @login_router.post("/criar_conta")
-async def criar_conta(email: str, senha: str, nome: str, session = Depends(pegar_sessao)): #FastAPI funciona melhor declarando o tipo de dado da variável
-    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+async def criar_conta(usuario_schema: UsuarioSchema, session: session = Depends(pegar_sessao)): #FastAPI funciona melhor declarando o tipo de dado da variável
+    usuario = session.query(Usuario).filter(Usuario.email==usuario_schema.email).first()
     if usuario:
         raise HTTPException(status_code=400, detail="Email já cadastrado") #retorna como erro, não com status code de 200
     else:
-        senha_criptografada = bcrypt_context.hash(senha)
-        novo_usuario = Usuario(nome=nome, email=email, senha=senha_criptografada)
+        senha_criptografada = bcrypt_context.hash(usuario_schema.senha)
+        novo_usuario = Usuario(nome=usuario_schema.nome, email=usuario_schema.email, senha=senha_criptografada, ativo=usuario_schema.ativo, admin=usuario_schema.admin)
         session.add(novo_usuario)
         session.commit()
-        return {"mensagem": f"Usuário cadastrado com sucesso {email}"}
+        return {"mensagem": f"Usuário cadastrado com sucesso {usuario_schema.email}"}

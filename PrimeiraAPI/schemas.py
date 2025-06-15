c86bd1c5e60242eb4778fcafce1a6775740e7df4
Dev Nadson <nadson.pros@gmail.com>
2025-06-15 03:26:07 -0300

    Feat: Schema dos produtos adicionados

--- PrimeiraAPI/schemas.py
+++ PrimeiraAPI/schemas.py
@@ -7,6 +7,10 @@ class UsuarioSchema(BaseModel):
     senha: str
     ativo: Optional[bool]
     admin: Optional[bool]
-    
+    class Config:
+        from_attributes = True
+
+class PedidoSchema(BaseModel):
+    id_usuario: int
     class Config:
         from_attributes = True

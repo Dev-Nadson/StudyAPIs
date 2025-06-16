f87ac6911bd2ce3ca4c85460705df93e1f0e82b0
Dev Nadson <nadson.pros@gmail.com>
2025-06-16 16:25:19 -0300

    Feat: Token previsório para testes, e rota de autenticação iniciada

--- PrimeiraAPI/login_routes.py
+++ PrimeiraAPI/login_routes.py
@@ -1,16 +1,21 @@
 from fastapi import APIRouter, HTTPException,Depends
 from models import Usuario
 from dependencies import pegar_sessao, bcrypt_context
-from schemas import UsuarioSchema
-from sqlalchemy.orm import session
+from schemas import UsuarioSchema, AutenticacaoSchema
+from sqlalchemy.orm import Session
 
 login_router = APIRouter(prefix="/login", tags=["login"])
+
+def criar_token(id_usuario):
+    token = f"12345{id_usuario}"
+    return token
+
 @login_router.get("/")
 async def home():
     return {"Mensagem": "Rota funcionando.", "Status": True}
 
 @login_router.post("/criar_conta")
-async def criar_conta(usuario_schema: UsuarioSchema, session: session = Depends(pegar_sessao)): #FastAPI funciona melhor declarando o tipo de dado da variável
+async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao)): #FastAPI funciona melhor declarando o tipo de dado da variável
     usuario = session.query(Usuario).filter(Usuario.email==usuario_schema.email).first()
     if usuario:
         raise HTTPException(status_code=400, detail="Email já cadastrado") #retorna como erro, não com status code de 200
@@ -20,3 +25,12 @@ async def criar_conta(usuario_schema: UsuarioSchema, session: session = Depends(
         session.add(novo_usuario)
         session.commit()
         return {"mensagem": f"Usuário cadastrado com sucesso {usuario_schema.email}"}
+
+#Iniciando a autenticação do login vis Json Web Token
+@login_router.post("/autenticacao")
+async def autenticacao(autenticacao_schema: AutenticacaoSchema, session: Session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email==autenticacao_schema.email).first()
+    if not usuario:
+        raise HTTPException(status_code=400, detail="Usuário não encontrado")
+    else:
+        access_token = criar_token(usuario.id)

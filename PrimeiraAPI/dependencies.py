498d90b6b3b98632902a6c67d613a8dbeecd513f
Dev Nadson <nadson.pros@gmail.com>
2025-06-17 10:52:30 -0300

    Feat: Novas variáveis .env adicionadas

--- PrimeiraAPI/dependencies.py
+++ PrimeiraAPI/dependencies.py
@@ -5,7 +5,11 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto") #garantir que a criptografia não seja obsoleta
 
 def pegar_sessao():

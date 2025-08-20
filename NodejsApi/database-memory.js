2902274c51f58b681553060f94f914d7f066a42e
Dev Nadson <nadson.pros@gmail.com>
2025-08-20 13:58:58 -0300

    Feat: Query search adicionada

--- NodejsApi/database-memory.js
+++ NodejsApi/database-memory.js
@@ -3,19 +3,33 @@ import { randomUUID } from 'node:crypto'
 export class DatabaseMemory {
     #tasks = new Map() //Map para poder fazer a conexão entre ID e Tarefa
 
-    async list() {
-        return Array.from(this.#tasks.values())
+    async list(search) {
+        return Array.from(this.#tasks.entries())
+            .map((TaskArray) => {
+                const id = TaskArray[0]
+                const data = TaskArray[1]
+
+                return {
+                    id,
+                    ...data
+                }
+            })
+            .filter(task => {
+                if (search) {
+                    return task.title.includes(search)
+                }
+                return true
+            })
     }
 
     async create(task) {
-        const taskID = randomUUID
+        const taskID = randomUUID()
         this.#tasks.set(taskID, task)
     }
 
     async update(id, task) {
         this.#tasks.set(id, task)
     }
-
     async delete(id) {
         this.#tasks.delete(id)
     }

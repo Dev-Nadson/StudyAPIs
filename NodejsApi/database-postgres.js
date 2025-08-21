c97a2bfcb4bf2508d4a0b03320a4965599074446
Dev Nadson <nadson.pros@gmail.com>
2025-08-21 17:44:17 -0300

    Commit Inicial

---
+++ NodejsApi/database-postgres.js
@@ -0,0 +1,13 @@
+import { randomUUID } from 'node:crypto'
+import sql from './db.js'
+
+export class DatabaseMemory {
+    async list(search) {
+    }
+    async create(task) {
+    }
+    async update(id, task) {
+    }
+    async delete(id) {
+    }
+}

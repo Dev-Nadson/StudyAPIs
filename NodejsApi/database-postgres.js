7d9cf7f88d271ceff838a28027af128327d30360
Dev Nadson <nadson.pros@gmail.com>
2025-08-23 00:07:41 -0300

    Feat: Rotas Create e List Concluídas

--- NodejsApi/database-postgres.js
+++ NodejsApi/database-postgres.js
@@ -1,10 +1,21 @@
 import { randomUUID } from 'node:crypto'
 import sql from './db.js'
 
-export class DatabaseMemory {
+export class DatabasePostgres {
     async list(search) {
+        let tasks
+        if (search) {
+            tasks = await sql`select * from tasks where title ilike ${'%' + search + '%'}` //ilike considera de todas as formas, seja maiusculo, minusculo, etc
+        } else {
+            tasks = await sql`select * from tasks`
+        }
+        return tasks
     }
     async create(task) {
+        const taskID = randomUUID()
+        const { title, description } = task
+
+        await sql`insert into tasks (id, title, description) VALUES (${taskID}, ${title}, ${description})`
     }
     async update(id, task) {
     }
